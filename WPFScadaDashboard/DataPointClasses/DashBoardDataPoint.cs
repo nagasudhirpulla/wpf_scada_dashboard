6fcd742bfab0396102f02627257bbe47ff675597
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-03-13 16:05:51 +0530

    just started

---
+++ WPFScadaDashboard/DataPointClasses/DashBoardDataPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFScadaDashboard.DataPointClasses
+{
+    class DashBoardDataPoint
+    {
+        // This is a generic dashboard DataPoint Class which other specific data point types are going to implement
+        public string Name_ { get; set; }
+        public string Id_ { get; set; }
+        public string PointType_ { get; set; } = "Generic";
+
+        // Constructor. We are not allowing to have null Id
+        public DashBoardDataPoint(string Id)
+        {
+            Id_ = Id;
+            // Keeping name = Id since we dont have a name supplied
+            Name_ = Id_;
+        }
+
+        // Constructor. We are not allowing to have null Id and Name Attributes
+        public DashBoardDataPoint(string Id, string Name)
+        {
+            Name_ = Name;
+            Id_ = Id;
+        }
+    }
+}

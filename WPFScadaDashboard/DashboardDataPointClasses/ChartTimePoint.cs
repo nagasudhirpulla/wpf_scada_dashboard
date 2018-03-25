9557fed18b03f70aea9553201355d6e04cbeac7e
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-03-26 02:36:33 +0530

    added new classes

---
+++ WPFScadaDashboard/DashboardDataPointClasses/ChartTimePoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class ChartTimePoint
+{
+    public double Val_ { get; set; }
+    public DateTime Time_ { get; set; }
+
+    public ChartTimePoint(double val_, DateTime time_)
+    {
+        Val_ = val_;
+        Time_ = time_;
+    }
+}

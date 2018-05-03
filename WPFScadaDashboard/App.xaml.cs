7965c0fb16be0c53c7397a925a1c88146a0817a1
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-05-03 09:31:45 +0530

    tried file not opening issue

--- WPFScadaDashboard/App.xaml.cs
+++ WPFScadaDashboard/App.xaml.cs
@@ -16,6 +16,7 @@ namespace WPFScadaDashboard
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            /*
             // https://stackoverflow.com/questions/26845815/how-to-pass-file-as-parameter-to-your-program-wpf-c-sharp
             this.Properties["FilePathArgName"] = null;
             var args = Environment.GetCommandLineArgs();
@@ -24,14 +25,43 @@ namespace WPFScadaDashboard
                 var fileName = args[1];
                 if (File.Exists(fileName))
                 {
-                    /*
-                    var extension = Path.GetExtension(fileName);
-                    if (extension == ".sudhir")
-                    {
-                        this.Properties["FilePathArgName"] = fileName;
-                    }
-                    */
+                    //
+                    //var extension = Path.GetExtension(fileName);
+                    //if (extension == ".sudhir")
+                    //{
+                    //    this.Properties["FilePathArgName"] = fileName;
+                    //}
+                    //
                     this.Properties["FilePathArgName"] = fileName;
+                }
+            }
+            */
+            //https://blogs.msdn.microsoft.com/avip/2008/10/26/wpf-supporting-command-line-arguments-and-file-extensions/
+            // Check if this was launched by double-clicking a doc. If so, use that as the
+            // startup file name.
+            if (AppDomain.CurrentDomain.SetupInformation
+                .ActivationArguments != null && AppDomain.CurrentDomain.SetupInformation
+                .ActivationArguments.ActivationData != null
+            && AppDomain.CurrentDomain.SetupInformation
+                .ActivationArguments.ActivationData.Length > 0)
+            {
+                string fname = "No filename given";
+                try
+                {
+                    fname = AppDomain.CurrentDomain.SetupInformation
+                            .ActivationArguments.ActivationData[0];
+
+                    // It comes in as a URI; this helps to convert it to a path.
+                    Uri uri = new Uri(fname);
+                    fname = uri.LocalPath;
+
+                    this.Properties["FilePathArgName"] = fname;
+
+                }
+                catch (Exception ex)
+                {
+                    // For some reason, this couldn't be read as a URI.
+                    // Do what you must...
                 }
             }
             base.OnStartup(e);
